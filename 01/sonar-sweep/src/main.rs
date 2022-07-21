b63a6b7ce61947e2e850dc05a07768b56ee0fc75
Karol Moroz <k.j.moroz@gmail.com>
2022-07-21 23:36:48 +0200

    Solve Sonar Sweep

--- 01/sonar-sweep/src/main.rs
+++ 01/sonar-sweep/src/main.rs
@@ -1,3 +1,27 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader, Lines};
+
+fn next_number(lines: &mut Lines<BufReader<File>>) -> Option<usize> {
+    if let Some(Ok(line)) = lines.next() {
+        return line.trim().parse().ok();
+    }
+    return None;
+}
+
 fn main() {
-    println!("Hello, world!");
+    let file = File::open("./input.txt").unwrap();
+    let file = BufReader::new(file);
+
+    let mut grow_count = 0;
+    let mut lines = file.lines();
+    let mut last_val = next_number(&mut lines).unwrap();
+
+    while let Some(value) = next_number(&mut lines) {
+        if value > last_val {
+            grow_count += 1;
+        }
+        last_val = value;
+    }
+
+    println!("{}", grow_count);
 }
